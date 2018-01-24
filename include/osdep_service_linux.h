f7b2a9a012c6db28430347c37e498cc64433fb20
jeremyb31 <jeremy53561@gmail.com>
2018-01-24 15:49:55 -0600

    \add patch

--- include/osdep_service_linux.h
+++ include/osdep_service_linux.h
@@ -45,7 +45,12 @@
 #include <linux/semaphore.h>
 #endif
 #include <linux/sem.h>
+
 #include <linux/sched.h>
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0))
+	#include <linux/sched/signal.h>
+#endif
+
 #include <linux/etherdevice.h>
 #include <linux/wireless.h>
 #include <net/iw_handler.h>
