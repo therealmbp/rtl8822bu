04abf4d089f1c93b40502f150f0a06e29006f2f1
jstach <jstach@uos.de>
2018-07-31 21:49:34 +0200

    Add support for Kernel 4.15, which changed the timer interface.

--- hal/phydm/phydm_types.h
+++ hal/phydm/phydm_types.h
@@ -153,8 +153,12 @@ typedef enum _RT_SPINLOCK_TYPE{
 
 	typedef struct rtl8192cd_priv	*prtl8192cd_priv;
 	typedef struct stat_info		STA_INFO_T,*PSTA_INFO_T;
-	typedef struct timer_list		RT_TIMER, *PRT_TIMER;
-	typedef  void *				RT_TIMER_CALL_BACK;
+#if defined (LINUX_VERSION_CODE) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0))
+        typedef struct legacy_timer_emu		RT_TIMER, *PRT_TIMER;
+#else
+        typedef struct timer_list		RT_TIMER, *PRT_TIMER;
+#endif
+        typedef  void *				RT_TIMER_CALL_BACK;
 
 #ifdef CONFIG_PCI_HCI
 	#define DEV_BUS_TYPE		RT_PCI_INTERFACE
@@ -223,8 +227,12 @@ typedef enum _RT_SPINLOCK_TYPE{
 		#define	ODM_ENDIAN_TYPE			ODM_ENDIAN_BIG
 	#endif
 	
-	typedef struct timer_list		RT_TIMER, *PRT_TIMER;
-	typedef  void *				RT_TIMER_CALL_BACK;
+#if defined (LINUX_VERSION_CODE) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0))
+        typedef struct legacy_timer_emu		RT_TIMER, *PRT_TIMER;
+#else
+        typedef struct timer_list		RT_TIMER, *PRT_TIMER;
+#endif
+        typedef  void *				RT_TIMER_CALL_BACK;
 	#define	STA_INFO_T			struct sta_info
 	#define	PSTA_INFO_T		struct sta_info *
 		
