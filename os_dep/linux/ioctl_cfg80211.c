5a0de4e833595680916945ec551e01fb376f6301
ElectricSolstice <repairagent@yahoo.com>
2019-01-04 11:24:53 -0800

    Deprecated get_monotonic_boottime replaced with ktime_get_boottime for Linux >= 3.17

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -321,7 +321,9 @@ static const struct ieee80211_txrx_stypes
 #endif
 
 static u64 rtw_get_systime_us(void) {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 17, 0))
+	return ktime_get_boottime();
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
 	struct timespec ts;
 	get_monotonic_boottime(&ts);
 	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
