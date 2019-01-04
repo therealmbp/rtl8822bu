aaa92d81642b427f22cd08d0e3acf47f32df9a9e
ElectricSolstice <repairagent@yahoo.com>
2019-01-04 12:30:25 -0800

    Fixed previous patch to use timespec64

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -322,7 +322,9 @@ static const struct ieee80211_txrx_stypes
 
 static u64 rtw_get_systime_us(void) {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 17, 0))
-	return ktime_get_boottime();
+	struct timespec64 ts;
+	ktime_get_boottime_ts64(&ts);
+	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
 	struct timespec ts;
 	get_monotonic_boottime(&ts);
